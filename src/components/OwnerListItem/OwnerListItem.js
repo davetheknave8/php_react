230ed2ccf62853765b8567c16194afa27c141721
David Reeves <davidsreeves1@gmail.com>
2019-08-21 15:09:59 -0500

    added new component Owner list item

---
+++ src/components/OwnerListItem/OwnerListItem.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class OwnerListItem extends Component{
+    render(){
+        return(
+            <>
+            </>
+        )
+    }
+}
+
+export default OwnerListItem;
--- src/components/Owners/Owners.js
+++ src/components/Owners/Owners.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
+import OwnerListItem from '../OwnerListItem/OwnerListItem';
+
+
 class Owners extends Component {
     componentDidMount = () => {
         this.props.dispatch({type: 'FETCH_OWNERS'});
@@ -9,7 +18,17 @@ class Owners extends Component {
     render() {
         console.log(this.props.reduxStore.ownerReducer);
         return (
-            <>
+            <> 
+            <Table>
+                <TableHead>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Number of Pets</TableCell>
+                    <TableCell>&nbsp;</TableCell>
+                </TableHead>
+                <TableBody>
+                    {this.props.reduxStore.ownerReducer.map((owner, i) => <OwnerListItem key={i} owner={owner} />)}
+                </TableBody>
+            </Table>
 
             </>
         )
