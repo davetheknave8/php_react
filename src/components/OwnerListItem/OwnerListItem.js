edd83a2bd5f113ea08628a5317cd92bcf4cf90db
David Reeves <davidsreeves1@gmail.com>
2019-08-21 15:58:50 -0500

    finished owners page

--- src/components/OwnerListItem/OwnerListItem.js
+++ src/components/OwnerListItem/OwnerListItem.js
@@ -1,12 +1,36 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import Button from '@material-ui/core/Button';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+    deleteBtn: {
+        backgroundColor: 'gray',
+        color: 'white'
+    }
+})
 
 class OwnerListItem extends Component{
+    handleDelete = () => {
+        this.props.dispatch({type: 'DELETE_OWNER', payload: this.props.owner.id})
+    }
+
     render(){
+        const {classes} = this.props;
         return(
             <>
+            <TableRow>
+                <TableCell>{this.props.owner.name}</TableCell>
+                <TableCell>{this.props.owner.count}</TableCell>
+                <TableCell><Button variant="contained" className={classes.deleteBtn} size="small" onClick={() => this.handleDelete()}>Delete Owner</Button></TableCell>
+            </TableRow>
+
             </>
         )
     }
 }
 
-export default OwnerListItem;
+export default withStyles(styles)(connect()(OwnerListItem));
