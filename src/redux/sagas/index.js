d635403af1dbcf0e40383471ca0f5e3aa5f273ce
David Reeves <davidsreeves1@gmail.com>
2019-08-22 11:40:42 -0500

    adding delete route

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -2,9 +2,11 @@ import { all, takeEvery } from 'redux-saga/effects';
 import getOwners from './getOwners';
 import getPets from './getPets';
 import addOwner from './addOwner';
+import deleteOwner from './deleteOwner';
 
 export default function* rootSaga(){
    yield takeEvery('FETCH_OWNERS', getOwners)
    yield takeEvery('FETCH_PETS', getPets)
    yield takeEvery('ADD_OWNER', addOwner)
+   yield takeEvery('DELETE_OWNER', deleteOwner);
 }
